b3f13adb25f8748119266e26acb111569a89f2b3
salsitu <sals@itu.dk>
2022-05-15 15:08:26 +0200

    Assignment 6

--- Eval.fs
+++ Eval.fs
@@ -1,134 +1,185 @@
 ﻿module Eval
 
-open StateMonad
+    open StateMonad
 
-(* Code for testing *)
+    (* Code for testing *)
 
-let hello =
-    [ ('H', 4)
-      ('E', 1)
-      ('L', 1)
-      ('L', 1)
-      ('O', 1) ]
+    let hello = [('H', 4); ('E', 1); ('L', 1); ('L', 1); ('O', 1);]
+    let state = mkState [("x", 5); ("y", 42)] hello ["_pos_"; "_result_"]
+    let emptyState = mkState [] [] []
+    
+    let add (a:SM<int>) (b:SM<int>) =
+        a >>=
+        (fun v1 -> b >>= (fun v2 -> ret(v1 + v2)))
+        
+    let sub (a:SM<int>) (b:SM<int>) =
+        a >>=
+        (fun v1 -> b >>= (fun v2 -> ret(v1 - v2)))
+        
+    let mul (a:SM<int>) (b:SM<int>) =
+        a >>=
+        (fun v1 -> b >>= (fun v2 -> ret(v1 * v2)))
+        
+    
+    let div a b =
+        a >>=
+        (fun v1 -> b >>= (fun v2 -> match v2 with
+                                    | v2 when v2 = 0 -> fail(DivisionByZero)
+                                    | _ -> ret(v1 / v2)))
+    
+    let modu a b =
+        a >>=
+        (fun v1 -> b >>= (fun v2 -> match v2 with
+                                    | v2 when v2 = 0 -> fail(DivisionByZero)
+                                    | _ -> ret(v1 % v2)))
 
-let state = mkState [ ("x", 5); ("y", 42) ] hello [ "_pos_"; "_result_" ]
-let emptyState = mkState [] [] []
+    type aExp =
+        | N of int
+        | V of string
+        | WL
+        | PV of aExp
+        | Add of aExp * aExp
+        | Sub of aExp * aExp
+        | Mul of aExp * aExp
+        | Div of aExp * aExp
+        | Mod of aExp * aExp
+        | CharToInt of cExp
+    and cExp =
+        | C  of char  (* Character value *)
+        | CV of aExp  (* Character lookup at word index *)
+        | ToUpper of cExp
+        | ToLower of cExp
+        | IntToChar of aExp
 
-let add a b = failwith "Not implemented"
-let div a b = failwith "Not implemented"
+    type bExp =             
+        | TT                   (* true *)
+        | FF                   (* false *)
+        | AEq of aExp * aExp   (* numeric equality *)
+        | ALt of aExp * aExp   (* numeric less than *)
+        | Not of bExp          (* boolean not *)
+        | Conj of bExp * bExp  (* boolean conjunction *)
+        | IsVowel of cExp      (* check for vowel *)
+        | IsLetter of cExp     (* check for letter *)
+        | IsDigit of cExp      (* check for digit *)
 
-type aExp =
-    | N of int
-    | V of string
-    | WL
-    | PV of aExp
-    | Add of aExp * aExp
-    | Sub of aExp * aExp
-    | Mul of aExp * aExp
-    | Div of aExp * aExp
-    | Mod of aExp * aExp
-    | CharToInt of cExp
+    let (.+.) a b = Add (a, b)
+    let (.-.) a b = Sub (a, b)
+    let (.*.) a b = Mul (a, b)
+    let (./.) a b = Div (a, b)
+    let (.%.) a b = Mod (a, b)
 
-and cExp =
-    | C of char (* Character value *)
-    | CV of aExp (* Character lookup at word index *)
-    | ToUpper of cExp
-    | ToLower of cExp
-    | IntToChar of aExp
+    let (~~) b = Not b
+    let (.&&.) b1 b2 = Conj (b1, b2)
+    let (.||.) b1 b2 = ~~(~~b1 .&&. ~~b2)       (* boolean disjunction *)
+    let (.->.) b1 b2 = (~~b1) .||. b2           (* boolean implication *) 
+       
+    let (.=.) a b = AEq (a, b)   
+    let (.<.) a b = ALt (a, b)   
+    let (.<>.) a b = ~~(a .=. b)
+    let (.<=.) a b = a .<. b .||. ~~(a .<>. b)
+    let (.>=.) a b = ~~(a .<. b)                (* numeric greater than or equal to *)
+    let (.>.) a b = ~~(a .=. b) .&&. (a .>=. b) (* numeric greater than *)    
 
-type bExp =
-    | TT (* true *)
-    | FF (* false *)
+    let rec arithEval : aExp -> SM<int> =
+        function
+        | N n -> ret n
+        | V v -> lookup v
+        | WL -> wordLength
+        | PV v -> arithEval v >>= pointValue
+        | Add (a, b) -> add (arithEval a)  (arithEval b)
+        | Sub (a, b) -> sub (arithEval a) (arithEval b)
+        | Mul (a, b) -> mul (arithEval a) (arithEval b)
+        | Div (a,b) -> div (arithEval a) (arithEval b)
+        | Mod (a, b) -> modu (arithEval a) (arithEval b)
+        | CharToInt c -> charEval c >>= (fun c -> ret(int(c)))
+    and charEval : cExp -> SM<char> =
+        function
+        | C c -> ret c
+        | CV a -> arithEval(a) >>= characterValue
+        | ToLower c -> charEval c >>= (System.Char.ToLower >> ret)
+        | ToUpper c -> charEval c >>= (System.Char.ToUpper >> ret)
+        | IntToChar i -> arithEval i >>= (fun i -> ret(char(i)))
 
-    | AEq of aExp * aExp (* numeric equality *)
-    | ALt of aExp * aExp (* numeric less than *)
+    let isVowel l =
+        match System.Char.ToUpper l with
+        | 'A' | 'E' | 'I' | 'O' | 'U' -> true
+        | _ -> false
+    
+    let rec boolEval : bExp -> SM<bool> =
+        function
+        | TT -> ret true
+        | FF -> ret false
+        | AEq (a, b) -> arithEval a >>= (fun i1 -> arithEval b >>= (fun i2 -> ret (i1 = i2)))
+        | ALt (a, b) -> arithEval a >>= (fun i1 -> arithEval b >>= (fun i2 -> ret (i1 < i2)))
+        | Not b -> boolEval b >>= (fun b -> ret(not b))
+        | Conj (a, b) -> boolEval a >>= (fun b1 -> boolEval b >>= (fun b2 -> ret (b1 && b2)))
+        | IsVowel c -> charEval c >>= (isVowel >> ret)
+        | IsLetter c -> charEval c >>= (System.Char.IsLetter >> ret)
+        | IsDigit c -> charEval c >>= (System.Char.IsDigit >> ret)
 
-    | Not of bExp (* boolean not *)
-    | Conj of bExp * bExp (* boolean conjunction *)
+    type stm =                    (* statements *)
+        | Declare of string       (* variable declaration *)
+        | Ass of string * aExp    (* variable assignment *)
+        | Skip                    (* nop *)
+        | Seq of stm * stm        (* sequential composition *)
+        | ITE of bExp * stm * stm (* if-then-else statement *)
+        | While of bExp * stm     (* while statement *)
 
-    | IsVowel of cExp (* check for vowel *)
-    | IsLetter of cExp (* check for letter *)
-    | IsDigit of cExp (* check for digit *)
-
-let (.+.) a b = Add(a, b)
-let (.-.) a b = Sub(a, b)
-let (.*.) a b = Mul(a, b)
-let (./.) a b = Div(a, b)
-let (.%.) a b = Mod(a, b)
-
-let (~~) b = Not b
-let (.&&.) b1 b2 = Conj(b1, b2)
-
-let (.||.) b1 b2 =
-    ~~(~~b1 .&&. ~~b2) (* boolean disjunction *)
-
-let (.->.) b1 b2 =
-    (~~b1) .||. b2 (* boolean implication *)
-
-let (.=.) a b = AEq(a, b)
-let (.<.) a b = ALt(a, b)
-let (.<>.) a b = ~~(a .=. b)
-let (.<=.) a b = a .<. b .||. ~~(a .<>. b)
-
-let (.>=.) a b =
-    ~~(a .<. b) (* numeric greater than or equal to *)
-
-let (.>.) a b =
-    ~~(a .=. b)
-    .&&. (a .>=. b) (* numeric greater than *)
-
-let arithEval a : SM<int> = failwith "Not implemented"
-
-let charEval c : SM<char> = failwith "Not implemented"
-
-let boolEval b : SM<bool> = failwith "Not implemented"
-
-
-type stm = (* statements *)
-    | Declare of string (* variable declaration *)
-    | Ass of string * aExp (* variable assignment *)
-    | Skip (* nop *)
-    | Seq of stm * stm (* sequential composition *)
-    | ITE of bExp * stm * stm (* if-then-else statement *)
-    | While of bExp * stm (* while statement *)
-
-let rec stmntEval stmnt : SM<unit> = failwith "Not implemented"
+    let rec stmntEval : stm -> SM<unit> =
+        function
+        | Declare s -> declare s
+        | Ass (s, a) -> arithEval a >>= update s
+        | Skip -> ret ()
+        | Seq (stm1, stm2) -> stmntEval stm1 >>>= stmntEval stm2
+        | ITE (b, stm1, stm2) -> boolEval b >>= function 
+                                                | true -> push >>>= stmntEval stm1 >>>= pop
+                                                | false -> push >>>= stmntEval stm2 >>>= pop
+        | While (b, stm) -> boolEval b >>= function
+                                           | true -> push >>>= stmntEval (While (b, stm)) >>>= pop
+                                           | false -> stmntEval Skip
 
 (* Part 3 (Optional) *)
 
-type StateBuilder() =
+    type StateBuilder() =
+        member this.Bind(f, x)    = f >>= x
+        member this.Return(x)     = ret x
+        member this.ReturnFrom(x) = x
+        member this.Delay(f)      = f ()
+        member this.Combine(a, b) = a >>= (fun _ -> b)
+        
+    let prog = new StateBuilder()
 
-    member this.Bind(f, x) = f >>= x
-    member this.Return(x) = ret x
-    member this.ReturnFrom(x) = x
-    member this.Delay(f) = f ()
-    member this.Combine(a, b) = a >>= (fun _ -> b)
+    let arithEval2 a = failwith "Not implemented"
+    let charEval2 c = failwith "Not implemented"
+    let rec boolEval2 b = failwith "Not implemented"
 
-let prog = new StateBuilder()
+    let stmntEval2 stm = failwith "Not implemented"
 
-let arithEval2 a = failwith "Not implemented"
-let charEval2 c = failwith "Not implemented"
-let rec boolEval2 b = failwith "Not implemented"
+(* Part 4 (Optional) *) 
 
-let stmntEval2 stm = failwith "Not implemented"
+    type word = (char * int) list
+    type squareFun = word -> int -> int -> Result<int, Error>
 
-(* Part 4 (Optional) *)
+    let stmntToSquareFun stm : squareFun = fun w pos acc ->
+        let vars = [("_pos_", pos); ("_acc_", acc); ("_result_", 0)]
+        stmntEval stm >>>= lookup "_result_" |> evalSM (mkState vars w (List.map fst vars))
 
-type word = (char * int) list
-type squareFun = word -> int -> int -> Result<int, Error>
+    type coord = int * int
+    type boardFun = coord -> Result<squareFun option, Error> 
 
-let stmntToSquareFun stm = failwith "Not implemented"
+    let stmntToBoardFun stm m : boardFun = fun coord ->
+        let vars = [("_x_", fst coord); ("_y_", snd coord); ("_result_", 0)]
+        match stmntEval stm >>>= lookup "_result_" |> evalSM (mkState vars [] (List.map fst vars)) with
+        | Failure err -> Failure err
+        | Success id -> match Map.tryFind id m with
+                        | Some sf -> Success (Some sf)
+                        | None    -> Success None
 
+    type board = {
+        center        : coord
+        defaultSquare : squareFun
+        squares       : boardFun
+    }
 
-type coord = int * int
-
-type boardFun = coord -> Result<squareFun option, Error>
-
-let stmntToBoardFun stm m = failwith "Not implemented"
-
-type board =
-    { center: coord
-      defaultSquare: squareFun
-      squares: boardFun }
-
-let mkBoard c defaultSq boardStmnt ids = failwith "Not implemented"
+    let mkBoard c defaultSq boardStmnt ids = failwith "Not implemented"
+    
--- StateMonad.fs
+++ StateMonad.fs
@@ -1,79 +1,91 @@
 ﻿module StateMonad
 
-type Error =
-    | VarExists of string
-    | VarNotFound of string
-    | IndexOutOfBounds of int
-    | DivisionByZero
-    | ReservedName of string
+    type Error = 
+        | VarExists of string
+        | VarNotFound of string
+        | IndexOutOfBounds of int
+        | DivisionByZero 
+        | ReservedName of string           
 
-type Result<'a, 'b> =
-    | Success of 'a
-    | Failure of 'b
+    type Result<'a, 'b>  =
+        | Success of 'a
+        | Failure of 'b
 
-type State =
-    { vars: Map<string, int> list
-      word: (char * int) list
-      reserved: Set<string> }
+    type State = {
+        vars     : Map<string, int> list
+        word     : (char * int) list 
+        reserved : Set<string>
+    }
 
-type SM<'a> = S of (State -> Result<'a * State, Error>)
+    type SM<'a> = S of (State -> Result<'a * State, Error>)
 
-let mkState lst word reserved =
-    { vars = [ Map.ofList lst ]
-      word = word
-      reserved = Set.ofList reserved }
+    let mkState lst word reserved = 
+           { vars = [Map.ofList lst];
+             word = word;
+             reserved = Set.ofList reserved }
 
-let evalSM (s: State) (S a: SM<'a>) : Result<'a, Error> =
-    match a s with
-    | Success (result, _) -> Success result
-    | Failure error -> Failure error
-
-let bind (f: 'a -> SM<'b>) (S a: SM<'a>) : SM<'b> =
-    S (fun s ->
+    let evalSM (s : State) (S a : SM<'a>) : Result<'a, Error> =
         match a s with
-        | Success (b, s') ->
-            match f b with
-            | S g -> g s'
-        | Failure err -> Failure err)
+        | Success (result, _) -> Success result
+        | Failure error -> Failure error
+
+    let bind (f : 'a -> SM<'b>) (S a : SM<'a>) : SM<'b> =
+        S (fun s ->
+              match a s with
+              | Success (b, s') -> 
+                match f b with 
+                | S g -> g s'
+              | Failure err     -> Failure err)
 
 
-let ret (v: 'a) : SM<'a> = S(fun s -> Success(v, s))
-let fail err : SM<'a> = S(fun s -> Failure err)
+    let ret (v : 'a) : SM<'a> = S (fun s -> Success (v, s))
+    let fail err     : SM<'a> = S (fun s -> Failure err)
 
-let (>>=) x f = bind f x
-let (>>>=) x f = x >>= (fun () -> f)
+    let (>>=)  x f = bind f x
+    let (>>>=) x f = x >>= (fun () -> f)
 
-let push: SM<unit> = S(fun s -> Success((), { s with vars = Map.empty :: s.vars }))
+    let push : SM<unit> = 
+        S (fun s -> Success ((), {s with vars = Map.empty :: s.vars}))
 
-let pop: SM<unit> = S(fun s -> Success((), { s with vars = s.vars.Tail }))
+    let pop : SM<unit> =
+        S (fun s -> Success ((), {s with vars = s.vars.[1..]}))
 
-let wordLength: SM<int> = S(fun s -> Success(List.length s.word, s))
+    let wordLength : SM<int> =
+        S (fun s -> Success (List.length s.word, {s with vars = s.vars}))
 
-let characterValue (pos: int) : SM<char> =
-    S (fun s ->
-        match List.tryItem pos s.word with
-        | Some v -> Success(fst v, s)
-        | None -> Failure(IndexOutOfBounds(pos)))
+    let characterValue (pos : int) : SM<char> =
+        S (fun s -> match pos with
+                    | pos when pos >= List.length s.word || pos < 0 -> Failure (IndexOutOfBounds pos)
+                    | _ -> Success(fst(s.word.[pos]), {s with vars = s.vars}))
 
-let pointValue (pos: int) : SM<int> =
-    S (fun s ->
-        match List.tryItem pos s.word with
-        | Some v -> Success(snd v, s)
-        | None -> Failure(IndexOutOfBounds(pos)))
+    let pointValue (pos : int) : SM<int> =
+        S (fun s -> match pos with
+                    | pos when pos >= List.length s.word || pos < 0 -> Failure (IndexOutOfBounds pos)
+                    | _ -> Success (snd(s.word.[pos]), {s with vars = s.vars}))
 
-let lookup (x: string) : SM<int> =
-    let rec aux =
-        function
-        | [] -> None
-        | m :: ms ->
-            match Map.tryFind x m with
-            | Some v -> Some v
-            | None -> aux ms
+    let lookup (x : string) : SM<int> = 
+        let rec aux =
+            function
+            | []      -> None
+            | m :: ms -> 
+                match Map.tryFind x m with
+                | Some v -> Some v
+                | None   -> aux ms
 
-    S (fun s ->
-        match aux (s.vars) with
-        | Some v -> Success(v, s)
-        | None -> Failure(VarNotFound x))
+        S (fun s -> 
+                match aux (s.vars) with
+                | Some v -> Success (v, s)
+                | None   -> Failure (VarNotFound x))
 
-let declare (var: string) : SM<unit> = failwith "Not implemented"
-let update (var: string) (value: int) : SM<unit> = failwith "Not implemented"
+    let declare (var : string) : SM<unit> =
+        S (fun s -> match Set.contains var s.reserved with
+                    | true  -> Failure (ReservedName var)
+                    | false -> match evalSM s (lookup var) with
+                               | Success _ -> Failure (VarExists var)
+                               | Failure _ -> Success ((), {s with vars = Map.add var 0 s.vars.Head :: s.vars.Tail}))
+
+    let update (var : string) (value : int) : SM<unit> =
+        S (fun s -> match evalSM s (lookup var) with
+                    | Success _ -> Success ((), {s with vars = Map.add var value s.vars.Head :: s.vars.Tail})
+                    | Failure _ -> Failure (VarNotFound var))
+              
